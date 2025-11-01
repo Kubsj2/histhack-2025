19e8b85d44f5e5feeca47f56b73f1136ccaba5c8
Jacpio <jacpio512383881@gmail.com>
2025-11-01 21:17:20 +0100

    Add animations to hand and moving

--- Assets/Script/HandAnimationController.cs
+++ Assets/Script/HandAnimationController.cs
@@ -1,18 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private static readonly int Trigger = Animator.StringToHash("Trigger");
+    private static readonly int Grip = Animator.StringToHash("Grip");
     [SerializeField] private InputActionProperty triggerAction;
     [SerializeField] private InputActionProperty gripAction;
+    [SerializeField] private Animator anim;
 
-    private Animator _anim;
     void Update()
     {
         float triggerValue = triggerAction.action.ReadValue<float>();
         float gripValue = gripAction.action.ReadValue<float>();
         
-        _anim.SetFloat("Trigger", triggerValue);
-        _anim.SetFloat("Grip", gripValue);
+        anim.SetFloat(Trigger, triggerValue);
+        anim.SetFloat(Grip, gripValue);
     }
 }
